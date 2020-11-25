a85ac2fff5e69e4c4bf5c704f2aab0536fe25b80
gaowei.liu <gaowei.liu@ly.com>
2020-11-25 10:48:49 +0800

    order-model

---
+++ order-model/src/main/java/com/ly/traffic/middleplatform/domain/changeticket/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @Package com.ly.traffic.middleplatform.domain.changeticket
+ * @Description: 改签聚合领域模型
+ * @author liugw
+ * @date 2020/8/27 18:09
+ */
+package com.ly.traffic.middleplatform.domain.changeticket;
